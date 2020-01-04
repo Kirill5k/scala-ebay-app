330aea9e9512b15efbec1587d1e7897cddb7ee98
kirill5k <immotional@aol.com>
2020-01-04 20:55:51 +0000

    find resell price method template

--- app/clients/cex/CexClient.scala
+++ app/clients/cex/CexClient.scala
@@ -1,5 +1,6 @@
 package clients.cex
 
+import configs.CexConfig
 import domain.ResellPrice
 import javax.inject.Inject
 import play.api.Configuration
@@ -9,7 +10,11 @@ import scala.concurrent.Future
 
 class CexClient @Inject() (config: Configuration, client: WSClient) {
 
-  def findResellPrice(query: String): Future[Either[Throwable, ResellPrice]] = {
+  private var cexConfig = config.get[CexConfig]("cex")
+  private var searchRequest = client
+    .url(s"${cexConfig.baseUri}${cexConfig.searchPath}")
+    .addHttpHeaders("Accept" -> "application/json")
+    .addHttpHeaders("Content-Type" -> "application/json")
 
-  }
+  def findResellPrice(query: String): Future[Either[Throwable, ResellPrice]] = ???
 }
--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@
 name := """scala-ebay-app"""
 organization := "io.kirill"
 
-version := "1.0-SNAPSHOT"
+version := "1.0"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
