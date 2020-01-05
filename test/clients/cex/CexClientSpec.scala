37137d161aa1368f36d0aa4da2b24bbd9f69a2eb
kirill5k <immotional@aol.com>
2020-01-05 18:10:37 +0000

    refactored cexclient

--- test/clients/cex/CexClientSpec.scala
+++ test/clients/cex/CexClientSpec.scala
@@ -6,12 +6,13 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.PlaySpec
 import play.api.{BuiltInComponentsFromContext, Configuration}
 import play.api.libs.json.Json
-import play.api.mvc.Results
+import play.api.mvc.{Result, Results}
 import play.api.routing.Router
 import play.api.test.WsTestClient
 import play.core.server.Server
 import play.api.routing.sird._
 import play.filters.HttpFiltersComponents
+
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
@@ -24,24 +25,29 @@ class CexClientSpec extends PlaySpec with ScalaFutures {
 
   "CexClient" should {
     "find minimal resell price" in {
-      Server.withApplicationFromContext() { context =>
-        new BuiltInComponentsFromContext(context) with HttpFiltersComponents {
-          override def router: Router = Router.from {
-            case GET(p"/cex/search" ? q"q=$query") =>
-              Action { req =>
-                Results.Ok.sendResource("cex/cex-search-response.json")(executionContext, fileMimeTypes)
-              }
-          }
-        }.application
-      } { implicit port =>
-        WsTestClient.withClient { client =>
-          val cexClient = new CexClient(config, client)
-          val result = cexClient.findResellPrice("iphone 7")
+      withCexClient(200, "cex/search-success-response.json") { cexClient =>
+        val result = cexClient.findResellPrice("iphone 7")
 
-          whenReady(result, timeout(6 seconds), interval(500 millis)) { minPrice =>
-            minPrice must be (Right(ResellPrice(BigDecimal.valueOf(108), BigDecimal.valueOf(153))))
-          }
+        whenReady(result, timeout(6 seconds), interval(500 millis)) { minPrice =>
+          minPrice must be (Right(ResellPrice(BigDecimal.valueOf(108), BigDecimal.valueOf(153))))
         }
+      }
+    }
+  }
+
+  def withCexClient[T](status: Int, responseFile: String)(block: CexClient => T): T = {
+    Server.withApplicationFromContext() { context =>
+      new BuiltInComponentsFromContext(context) with HttpFiltersComponents {
+        override def router: Router = Router.from {
+          case GET(p"/cex/search" ? q"q=$query") =>
+            Action { _ =>
+              Results.Status(status).sendResource(responseFile)(executionContext, fileMimeTypes)
+            }
+        }
+      }.application
+    } { implicit port =>
+      WsTestClient.withClient { client =>
+        block(new CexClient(config, client))
       }
     }
   }
