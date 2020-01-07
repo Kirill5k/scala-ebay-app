2af5ae608051398417b79249948caa5020f8acd2
kirill <kbibik@jaguarlandrover.com>
2020-01-07 08:59:13 +0000

    refactored cexclient

--- app/clients/cex/CexClient.scala
+++ app/clients/cex/CexClient.scala
@@ -1,5 +1,6 @@
 package clients.cex
 
+import cats.data.EitherT
 import configs.CexConfig
 import domain.ResellPrice
 import exceptions.ApiClientError
@@ -14,6 +15,8 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class CexClient @Inject() (config: Configuration, client: WSClient)(implicit ex: ExecutionContext) {
 
+  type FutureEither[A] = EitherT[Future, ApiClientError, A]
+
   private val logger: Logger = Logger(getClass)
 
   private val cexConfig = config.get[CexConfig]("cex")
@@ -23,24 +26,24 @@ class CexClient @Inject() (config: Configuration, client: WSClient)(implicit ex:
     .addHttpHeaders("Content-Type" -> "application/json")
 
   def findResellPrice(query: String): Future[Either[Throwable, ResellPrice]] = {
-    searchRequest
-      .withQueryStringParameters("q" -> query)
-      .get()
+    val searchResponse = searchRequest.withQueryStringParameters("q" -> query).get()
       .map(res =>
         if (Status.isSuccessful(res.status)) Right(res.body[JsValue].as[CexSearchResponse])
         else Left(ApiClientError(res.status, s"error sending request to cex: ${res.statusText}"))
       )
-      .map(cexSearchResponse => cexSearchResponse.map(_.response.data.boxes))
-      .map(searchResults => searchResults.map(boxes => {
-        logger.info(s"search '$query' returned ${boxes.size} results")
-        calcMinResellPrice(boxes)
-      }))
       .recover {
         case error: Throwable => Left(ApiClientError(Status.INTERNAL_SERVER_ERROR, error.getMessage))
       }
+
+    searchResponse
+      .map(cexSearchResponse => cexSearchResponse.map(_.response.data.boxes))
+      .map(searchResults => searchResults.map(boxes => {
+        logger.info(s"search '$query' returned ${boxes.size} results")
+        findMinResellPrice(boxes)
+      }))
   }
 
-  private def calcMinResellPrice(searchResults: Seq[SearchResult]): ResellPrice = {
+  private def findMinResellPrice(searchResults: Seq[SearchResult]): ResellPrice = {
     if (searchResults.isEmpty) ResellPrice.empty()
     else {
       val minPriceSearchResult: SearchResult = searchResults.minBy(_.exchangePrice)
