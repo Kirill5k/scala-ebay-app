86d6bdee8963ec656b0888cdf87af612db2d0f45
kirill <immotional@aol.com>
2020-01-13 11:14:59 +0000

    updated domain models

--- app/domain/ResellableItem.scala
+++ app/domain/ResellableItem.scala
@@ -2,7 +2,6 @@ package domain
 
 import java.time.Instant
 
-
 case class ResellPrice(cash: BigDecimal, exchange: BigDecimal)
 
 case class ListingDetails(
@@ -15,5 +14,38 @@ case class ListingDetails(
                            price: BigDecimal,
                            condition: String,
                            datePosted: Instant,
-                           dateEnded: Option[Instant],
+                           properties: Map[String, String],
+                           dateEnded: Option[Instant]
                          )
+
+sealed trait ItemDetails
+
+final case class PhoneDetails(
+                         make: Option[String],
+                         model: Option[String],
+                         colour: Option[String],
+                         manufacturerColour: Option[String],
+                         storageCapacity: Option[String],
+                         network: Option[String],
+                         condition: Option[String],
+                         mpn: Option[String]
+                       ) extends ItemDetails
+
+final case class GameDetails(
+                        name: Option[String],
+                        platform: Option[String],
+                        releaseYear: Option[String],
+                        genre: Option[String]
+                      ) extends ItemDetails
+
+sealed trait ResellableItem {
+  def itemDetails: ItemDetails
+  def listingDetails: ListingDetails
+  def resellPrice: Option[ResellPrice]
+}
+
+object ResellableItem {
+  final case class VideoGame(itemDetails: GameDetails, listingDetails: ListingDetails, resellPrice: Option[ResellPrice]) extends ResellableItem
+  final case class MobilePhone(itemDetails: PhoneDetails, listingDetails: ListingDetails, resellPrice: Option[ResellPrice]) extends ResellableItem
+
+}
