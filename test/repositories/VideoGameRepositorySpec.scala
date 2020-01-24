d9aa2b334dd00b6069f468f3f75d2a5dc1511ed3
kirill5k <immotional@aol.com>
2020-01-24 19:07:35 +0000

    PlayWithMongoSpec

---
+++ test/repositories/VideoGameRepositorySpec.scala
@@ -0,0 +1,11 @@
+package repositories
+
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import org.scalatestplus.play.PlaySpec
+
+import reactivemongo.play.json._
+
+class VideoGameRepositorySpec extends PlayWithMongoSpec with BeforeAndAfter {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+}
