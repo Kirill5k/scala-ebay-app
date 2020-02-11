2382672c7e93b395a10f7be84109d91e3046ac10
kirill <immotional@aol.com>
2020-02-11 14:14:45 +0000

    refactored ResellableItemService to use streams

--- app/services/ResellableItemService.scala
+++ app/services/ResellableItemService.scala
@@ -1,18 +1,21 @@
 package services
 
-import cats.data.EitherT
-import cats.implicits._
+
+import cats.effect.{IO, Timer}
 import clients.cex.CexClient
 import clients.ebay.EbaySearchClient
 import clients.telegram.TelegramClient
-import domain.ApiClientError.IOErrorOr
 import domain.{ItemDetails, ListingDetails, ResellPrice, ResellableItem}
+import fs2.Stream
 import repositories.{ResellableItemEntity, ResellableItemRepository}
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 trait ResellableItemService[I <: ResellableItem, D <: ItemDetails, E <: ResellableItemEntity] {
   implicit protected def ex: ExecutionContext
+  implicit private val timer: Timer[IO] = IO.timer(ex)
 
   protected def itemRepository: ResellableItemRepository[I, E]
   protected def ebaySearchClient: EbaySearchClient[D]
@@ -21,22 +24,20 @@ trait ResellableItemService[I <: ResellableItem, D <: ItemDetails, E <: Resellab
 
   protected def createItem(itemDetails: D, listingDetails: ListingDetails, resellPrice: Option[ResellPrice]): I
 
-  def getLatestFromEbay(minutes: Int): IOErrorOr[Seq[I]] =
-    ebaySearchClient.getItemsListedInLastMinutes(minutes).flatMap { itemDetails =>
-      itemDetails.map {
-        case (id, ld) => cexClient.findResellPrice(id).map(createItem(id, ld, _))
-      }.toList.sequence
-    }.map(_.toSeq)
+  def getLatestFromEbay(minutes: Int): Stream[IO, I] =
+    ebaySearchClient.getItemsListedInLastMinutes(minutes)
+      .delayBy(400 milliseconds)
+      .evalMap { case (id, ld) => cexClient.findResellPrice(id).map(rp => createItem(id, ld, rp)) }
 
-  def sendNotification(item: I): IOErrorOr[Unit] =
+  def sendNotification(item: I): IO[Unit] =
     telegramClient.sendMessageToMainChannel(item)
 
-  def save(item: I): IOErrorOr[Unit] =
+  def save(item: I): IO[Unit] =
     itemRepository.save(item)
 
-  def getLatest(limit: Int): IOErrorOr[Seq[I]] =
+  def getLatest(limit: Int): IO[Seq[I]] =
     itemRepository.findAll(limit)
 
-  def isNew(item: I): IOErrorOr[Boolean] =
+  def isNew(item: I): IO[Boolean] =
     itemRepository.existsByUrl(item.listingDetails.url).map(!_)
 }
