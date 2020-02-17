b7f9671d58ea825094a6b3ceec7a4dfa284a5f37
kirill <immotional@aol.com>
2020-02-17 11:19:35 +0000

    updated video game title filter

--- app/clients/ebay/mappers/GameDetailsMapper.scala
+++ app/clients/ebay/mappers/GameDetailsMapper.scala
@@ -9,7 +9,7 @@ private[mappers] object GameDetailsMapper {
   private val TITLE_WORDS_FILTER = List(
     "remastered", "playstation 4", "Nintendo switch", "sony", "ps4", "blu-ray", "Mirror", "New and sealed",
     "Brand new", "Factory Sealed", "Sealed", "Game new", ",", "Microsoft", "Free post", "Used", "xbox one", "Uk pal", "Game code",
-    "Hits", "Tom clancys", "Great Condition", "Videogame fasting", "switch", "new game",
+    "Hits", "Tom clancys", "Great Condition", "Videogame fasting", "switch", "new game", "premium",
     "[^\\p{L}\\p{N}\\p{P}\\p{Z}]"
   ).mkString("(?i)", "|", "")
 
