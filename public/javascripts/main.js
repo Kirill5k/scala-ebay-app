c2579c5ab1871e29e93a3dc4ed6e678f8d010b6d
kirill <immotional@aol.com>
2020-02-19 15:18:23 +0000

    make stats hidden initially

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,13 +4,14 @@ const limit = 10000
 const reject = err => Promise.reject(new Error(err))
 
 const displayStats = games => {
-  const total = games.length
-  const withoutResellPrice = games.filter(g => !g.resellPrice).length
-  const withGoodPrice = games.filter(g => g.resellPrice && g.listingDetails.price < g.resellPrice.cash).length
-  const videoGamesStats = document.getElementById("video-games")
-  videoGamesStats.querySelectorAll(".badge")[0].innerHTML = total
-  videoGamesStats.querySelectorAll(".badge")[1].innerHTML = withoutResellPrice
-  videoGamesStats.querySelectorAll(".badge")[2].innerHTML = withGoodPrice
+  const total = games.length;
+  const withoutResellPrice = games.filter(g => !g.resellPrice).length;
+  const withGoodPrice = games.filter(g => g.resellPrice && g.listingDetails.price < g.resellPrice.cash).length;
+  const videoGamesStats = document.getElementById("video-games");
+  videoGamesStats.querySelectorAll(".badge")[0].innerHTML = total;
+  videoGamesStats.querySelectorAll(".badge")[1].innerHTML = withoutResellPrice;
+  videoGamesStats.querySelectorAll(".badge")[2].innerHTML = withGoodPrice;
+  videoGamesStats.style.display = "block";
 }
 
 fetch(`/api/video-games?limit=${limit}&from=${from}`)
