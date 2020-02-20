788e4c14db32b352c05a3c3cade9cab2df50c1dd
kirill <immotional@aol.com>
2020-02-20 09:57:50 +0000

    cleaned ResellableItemRepository 2

--- app/repositories/ResellableItemRepository.scala
+++ app/repositories/ResellableItemRepository.scala
@@ -25,47 +25,38 @@ trait ResellableItemRepository[D <: ResellableItem, E <: ResellableItemEntity] {
   protected def collectionName: String
   protected val itemCollection: Future[JSONCollection] = mongo.database.map(_.collection(collectionName))
 
-  def existsByUrl(listingUrl: String): IO[Boolean] = {
-    val result = itemCollection.flatMap { collection =>
+  def existsByUrl(listingUrl: String): IO[Boolean] =
+    toIO(itemCollection.flatMap { collection =>
       collection
         .withReadPreference(ReadPreference.primary)
         .count(Some(Json.obj("listingDetails.url" -> listingUrl.toString)), None, 0, None, ReadConcern.Available)
         .map(_ > 0)
-        .map(_.asRight[ApiClientError])
-    }
-      .recover(ApiClientError.recoverFromDbError.andThen(_.asLeft))
-    ApiClientError.fromFutureErrorToIO(result)
-  }
+    })
 
   def save(item: D): IO[Unit] =
     (entityMapper.toEntity _ andThen saveEntity)(item)
 
-  private def saveEntity(entity: E): IO[Unit] = {
-    val result = itemCollection.flatMap{ collection =>
+  private def saveEntity(entity: E): IO[Unit] =
+    toIO(itemCollection.flatMap{ collection =>
       collection
-        .insert(ordered = false).
-        one(entity)
-    }
-      .map(_ => ().asRight[ApiClientError])
-      .recover(ApiClientError.recoverFromDbError.andThen(_.asLeft))
-    ApiClientError.fromFutureErrorToIO(result)
-  }
+        .insert(ordered = false)
+        .one(entity)
+    }) *> IO.pure(())
 
   def findAll(limit: Option[Int] = None, from: Option[Instant] = None): IO[Seq[D]] =
     findAllEntities(limit, from).map(_.map(entityMapper.toDomain))
 
-  private def findAllEntities(limit: Option[Int], from: Option[Instant]): IO[Seq[E]] = {
-    val result = itemCollection.flatMap { collection =>
+  private def findAllEntities(limit: Option[Int], from: Option[Instant]): IO[Seq[E]] =
+    toIO(itemCollection.flatMap { collection =>
       collection
         .find(selector = from.fold(BSONDocument())(postedAfterSelector), projection = Option.empty[JsObject])
         .sort(Json.obj("listingDetails.datePosted" -> -1))
-        .cursor[E]()
+        .cursor[E](ReadPreference.primary)
         .collect[List](limit.getOrElse(-1), Cursor.FailOnError[List[E]]())
-    }
-      .map(_.asRight)
-      .recover(ApiClientError.recoverFromDbError.andThen(_.asLeft))
-    ApiClientError.fromFutureErrorToIO(result)
-  }
+    })
+
+  private def toIO[A](result: Future[A]): IO[A] =
+    IO.fromFuture(IO(result)).handleErrorWith(error => IO.raiseError(ApiClientError.recoverFromDbError(error)))
 
   private def postedAfterSelector(from: Instant): BSONDocument =
     BSONDocument("listingDetails.datePosted" -> BSONDocument("$gte" -> BSONString(from.toString)))
