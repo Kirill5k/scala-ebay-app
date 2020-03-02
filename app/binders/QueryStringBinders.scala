a0e2d99b778574fb5c57d6a45a9e20dda401a9e5
kirill <immotional@aol.com>
2020-03-02 14:33:34 +0000

    updated date parsing

--- app/binders/QueryStringBinders.scala
+++ app/binders/QueryStringBinders.scala
@@ -1,6 +1,6 @@
 package binders
 
-import java.time.Instant
+import java.time.{Instant, LocalDate, ZoneOffset}
 
 import play.api.mvc.QueryStringBindable
 
@@ -8,10 +8,14 @@ import scala.util.{Either, Try}
 
 object QueryStringBinders {
   implicit def bindableInstant(implicit stringBinder: QueryStringBindable[String]): QueryStringBindable[Instant] = new QueryStringBindable[Instant] {
-    override def bind(key: String, params: Map[String,Seq[String]]): Option[Either[String, Instant]] = {
+    override def bind(key: String, params: Map[String,Seq[String]]): Option[Either[String, Instant]] =
       params.get(key)
         .flatMap(_.headOption)
-        .map(dateString => Try(Instant.parse(dateString)).toOption.toRight(s"Invalid date format: $dateString"))
+        .map(parseDate)
+
+    private def parseDate(dateString: String): Either[String, Instant] = {
+      val date  = if (dateString.length == 10) Try(LocalDate.parse(dateString)).map(_.atStartOfDay(ZoneOffset.UTC).toInstant) else Try(Instant.parse(dateString))
+      date.toOption.toRight(s"Invalid date format: $dateString")
     }
 
     override def unbind(key: String, date: Instant): String =
