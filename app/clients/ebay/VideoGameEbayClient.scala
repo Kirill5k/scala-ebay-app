763f668fc32611205521e0cb159a2c5c8c8d0f49
kirill <immotional@aol.com>
2020-03-13 09:39:01 +0000

    updates to video games filter

--- app/clients/ebay/VideoGameEbayClient.scala
+++ app/clients/ebay/VideoGameEbayClient.scala
@@ -22,7 +22,7 @@ class VideoGameEbayClient @Inject()(val ebayAuthClient: EbayAuthClient, val ebay
   private val LISTING_NAME_TRIGGER_WORDS = List(
     "digital download", "digital code", "digitalcode", "download code", "upgrade code", "game code", "style covers", "credits", "digital game", "game disc",
     "coin", "skins", "bundle", "no game", "digital key", "download key", "collectors box",
-    "Software Only", "cartridge only", "disc only", "game only", "cover only", "box only",
+    "Software Only", "cartridge only", "cart only", "disc only", "game only", "cover only", "box only",
     "cover case", "carry case", "travel case", "carrying case", "just the case", "no case", "carry bag",
     "toycon", "toy con", "dualshock", "efigs", "gamepad", "joycon", "joy con", "controller", "headset", "\\bhdmi\\b", "\\busb\\b",
     "player generator", "100k", "50k", "case box", "dlc", "pve", "starter pack",
