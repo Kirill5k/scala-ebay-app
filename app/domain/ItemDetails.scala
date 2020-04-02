096830c20b5b48b453efe4dea24343c289613369
kirill5k <immotional@aol.com>
2020-04-02 08:28:55 +0100

    update of ItemDetail to include new itemDetailType field

--- app/domain/ItemDetails.scala
+++ app/domain/ItemDetails.scala
@@ -2,29 +2,37 @@ package domain
 
 import cats.implicits._
 
+sealed trait ItemDetailsType
+
+object ItemDetailsType {
+  final case object Single extends ItemDetailsType
+  final case object Bundle extends ItemDetailsType
+}
+
 sealed trait ItemDetails {
+  def detailsType: ItemDetailsType
   def summary: Option[String]
 }
 
 object ItemDetails {
 
   final case class PhoneDetails(
-                                 make: Option[String],
-                                 model: Option[String],
-                                 colour: Option[String],
-                                 storageCapacity: Option[String],
-                                 network: Option[String],
-                                 condition: Option[String]
-                               ) extends ItemDetails {
+    make: Option[String],
+    model: Option[String],
+    colour: Option[String],
+    storageCapacity: Option[String],
+    network: Option[String],
+    condition: Option[String],
+    detailsType: ItemDetailsType = ItemDetailsType.Single) extends ItemDetails {
     val summary: Option[String] = List(make, model, storageCapacity, colour, network).sequence.map(_.mkString(" "))
   }
 
   final case class GameDetails(
-                                name: Option[String],
-                                platform: Option[String],
-                                releaseYear: Option[String],
-                                genre: Option[String]
-                              ) extends ItemDetails {
+    name: Option[String],
+    platform: Option[String],
+    releaseYear: Option[String],
+    genre: Option[String],
+    detailsType: ItemDetailsType = ItemDetailsType.Single) extends ItemDetails {
     val summary: Option[String] = name.flatMap(n => platform.map(p => s"$n $p"))
   }
 
--- app/repositories/ResellableItemEntity.scala
+++ app/repositories/ResellableItemEntity.scala
@@ -1,7 +1,7 @@
 package repositories
 
 import domain.ItemDetails.GameDetails
-import domain.{ItemDetails, ListingDetails, ResellPrice}
+import domain.{ItemDetails, ItemDetailsType, ListingDetails, ResellPrice}
 import reactivemongo.bson.BSONObjectID
 import reactivemongo.play.json._
 
@@ -13,16 +13,25 @@ trait ResellableItemEntity {
 }
 
 object ResellableItemEntity {
-  case class VideoGameEntity(
-                              _id: Option[BSONObjectID],
-                              itemDetails: GameDetails,
-                              listingDetails: ListingDetails,
-                              resellPrice: Option[ResellPrice]
-                            ) extends ResellableItemEntity
+  final case class VideoGameEntity(
+    _id: Option[BSONObjectID],
+    itemDetails: GameDetails,
+    listingDetails: ListingDetails,
+    resellPrice: Option[ResellPrice]) extends ResellableItemEntity
 
 
   import play.api.libs.json._
 
+  implicit val itemDetailsFormat: Format[ItemDetailsType] = new Format[ItemDetailsType] {
+    override def reads(json: JsValue): JsResult[ItemDetailsType] = json.toString() match {
+      case "bundle" => JsSuccess(ItemDetailsType.Bundle)
+      case _ => JsSuccess(ItemDetailsType.Single)
+    }
+    override def writes(detailsType: ItemDetailsType): JsString = detailsType match {
+      case ItemDetailsType.Bundle => JsString("bundle")
+      case _ => JsString("single")
+    }
+  }
   implicit val resellPriceFormat: OFormat[ResellPrice] = Json.format[ResellPrice]
   implicit val listingDetailsFormat: OFormat[ListingDetails] = Json.format[ListingDetails]
   implicit val videoGameDetailsFormat: OFormat[GameDetails] = Json.format[GameDetails]
