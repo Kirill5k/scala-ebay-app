b30cf646957f05b85d9bc73e653c2125c3a5dd44
kirill5k <immotional@aol.com>
2020-06-21 15:12:54 +0100

    updates to game details mapper

--- app/clients/ebay/mappers/GameDetailsMapper.scala
+++ app/clients/ebay/mappers/GameDetailsMapper.scala
@@ -13,7 +13,7 @@ private[mappers] object GameDetailsMapper {
     "(\\bHD\b|exclusive|limited collectors|definitive|atlas|platinum|complete|standard|std|classic(s)?|(\\d+(th)?)? anniversary|remastered|elite|\\beu\\b|coll(ector(s)?)?|ltd|goty|(action )?game of the|legacy( pro)?|unlimited|premium|(digital )?deluxe|ultimat)(?s).* (collection|edition|\\be(d)?(i)?(t)?(i)?\\b)(?s).*$",
     "(the )?((new|pristine|inc)\\s+)?(super|cheap( )?)?(free|fast|quick)?(\\s+)?(and )?(super( )?)?(prompt|free|fast|quick|(next|same) day|tracked|speedy|worldwide)(?s).* (dispatch|ship(ping)?|post(age)?|delivery|p(\\s+)?p).*$",
     "(?<=.{12})((brand\\s+)?new(?s).*)?((factory |un)?sealed|unopened|shrinkwrapped)(?s).*$",
-    "(?<=.{20})\\b(single player|Family Fun|(kids )?adventure|console single|tactical|3rd-person|rpg|fps|survival|action|racing|role|wrestling|fighting)\\b(?s).* game(?s).*"
+    "(?<=.{20})\\b(single player|Family Fun|((kids|fun) )?adventure|console single|tactical|3rd-person|rpg|fps|survival|action|racing|role|wrestling|fighting)\\b(?s).* game(?s).*"
   ).mkString("(?i)", "|", "")
 
   private val LEVEL2_TITLE_WORDS_REPLACEMENTS = List(
@@ -22,7 +22,7 @@ private[mappers] object GameDetailsMapper {
     "(dbl|double|triple|twin|expansion) (pack|pk)",
     "(1st|2nd|first) class.*$", "(boxed|complete) (with|case)(?s).*$", "exclusive to(?s).*$", "((comes )?with|no|missing|inc(ludes|luding)?) (booklet|original|instructions|box|map|case|manual)(?s).*$", "(the )?disc(s)? (are|is|in)(?s).*$",
     "(new )?(((very|super) )?rare|limited run|(\\d+ )?new|pal|physical|great|boxed|full|complete|boxed( and)?\\s+complete) game(s)?( \\d+)?( new)?",
-    "(in )?(great|(very )?good|incredible|ex(cellent)?|amazing|mint|superb|working|perfect|used|(fully )?tested|immaculate) (working|good|(working )?order|cond(ition)?|value|prices)",
+    "(in )?(great|(very )?good|incredible|ex(cellent)?|amazing|mint|superb|working|perfect|used|(fully )?tested|immaculate|fantastic) (working|good|(working )?order|cond(ition)?|value|prices)",
     "Warner Bros", "ubisoft", "currys", "Take( |-)?(Two|2)( Interactive)?", "(EA|2k) (music|sports|games)", "James Camerons", "\\bTom clancy(s)?\\b",
     "Bethesda(s)?( Softworks)?", "Hideo Kojima", "(bandai )?namco", "rockstar games", "James Bond", "Activision", "Peter Jacksons", "Naughty Dog",
     "Microsoft( 20\\d\\d)?", "sony", "electronic arts", "nintendo", "square enix", "Dreamworks", "Disney Pixar", "WB Games", "Bend Studio",
@@ -51,12 +51,12 @@ private[mappers] object GameDetailsMapper {
   private val EDGE_WORDS_REPLACEMENTS = List(
     "^(\\s)?(((brand )?NEW|BNIB)\\s+)?(and )?SEALED",
     "^\\bMARVEL\\b", "^SALE", "NEW$", "^BOXED", "^SALE", "^NEW", "^best", "^software", "un( |-)?opened$", "rare$", "^rare",
-    "bargain$", "mint$", "\\bfor\\b$", "premium$", "very$", "\\bLIMITED\\b$", "tested$"
+    "bargain$", "mint$", "\\bfor\\b$", "premium$", "very$", "\\bLIMITED\\b$", "tested$", "\\bON\\b$", "\\bBY\\b$"
   ).mkString("(?i)", "|", "")
 
   private val PLATFORMS_MATCH_REGEX = List(
     "PS\\d", "PLAYSTATION(\\s+)?(\\d)",
-    "NINTENDO SWITCH", "SWITCH", "\\bWII U\\b", "\\bWII\\b",
+    "NINTENDO SWITCH", "SWITCH", "\\bWII( )?U\\b", "\\bWII\\b",
     "X( )?B(OX)?(\\s+)?(ONE|\\d+)", "X360", "XBOX"
   ).mkString("(?i)", "|", "").r
 
@@ -72,7 +72,8 @@ private[mappers] object GameDetailsMapper {
     "SONYPLAYSTATION2" -> "PS2",
     "PLAYSTATION2" -> "PS2",
     "SONYPLAYSTATION1" -> "PS1",
-    "SONYPLAYSTATION" -> "PS4",
+    "SONYPLAYSTATION" -> "PS",
+    "PLAYSTATION" -> "PS",
     "NINTENDOSWITCH" -> "SWITCH",
     "XBOX1" -> "XBOX ONE",
     "XBOX360" -> "XBOX 360",
@@ -107,7 +108,7 @@ private[mappers] object GameDetailsMapper {
       .replaceAll(LEVEL1_TITLE_WORDS_REPLACEMENTS, "")
       .replaceAll(LEVEL2_TITLE_WORDS_REPLACEMENTS, "")
       .replaceAll(LEVEL3_TITLE_WORDS_REPLACEMENTS, "")
-      .replaceFirst("(?i)(the )?\\w+(?=\\s+(\\be(d)?(i)?(t)?(i)?(o)?(n)?\\b|coll(ection)?)) (\\be(d)?(i)?(t)?(i)?(o)?(n)?\\b|coll(ection)?)(?s).*$", "")
+      .replaceFirst("(?i)(?<=.{5})(the )?\\w+(?=\\s+(\\be(d)?(i)?(t)?(i)?(o)?(n)?\\b|coll(ection)?)) (\\be(d)?(i)?(t)?(i)?(o)?(n)?\\b|coll(ection)?)(?s).*$", "")
       .replaceAll("é", "e")
       .replaceAll("(?i)playerunknown", "Player Unknown")
       .replaceAll("(?i)(littlebigplanet)", "Little Big Planet")
